468a43b862fcc18735fcec53802f736abab10dc9
rkubapl <kontakt@rkuba.pl>
2023-07-20 21:33:15 +0200

    Up

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import Activity from "./components/Activity";
 
 function Home() {
     const [data, setData] = useState({activities: []});
+    const [countdown, setCountdown] = useState({loaded: false});
 
     useEffect(() => {
         fetch(process.env.REACT_APP_API_URL + "/stats")
@@ -23,13 +24,53 @@ function Home() {
         return (formatNumber(date.getHours()) + ":" + formatNumber(date.getMinutes()) + ":" + formatNumber(date.getSeconds()) + " " + formatNumber(date.getDate()) + "/"+ formatNumber(date.getMonth()+1) + "/"+ date.getFullYear());
     }
 
+    var countDownDate = new Date("Sep 4, 2023 00:00:00").getTime();
+
+    function updateCountdown() {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        setCountdown({loaded: true, finished: false, days, hours, minutes, seconds, percent: ((days/72)*100).toFixed(2)});
+
+        if (distance < 0) {
+            clearInterval(x);
+            setCountdown({loaded: true, finished: true});
+        }
+    }
+
+    if(!countdown.loaded) {
+        updateCountdown()
+        // setCountdown({...countdown, percent: (countdown.days/72)*100})
+    }
+    var x = setInterval(updateCountdown, 1000);
+
+    function countAvg(data) {
+        const missing = data.goal - data.distance;
+
+        if(missing > 0) {
+            return (missing/countdown.days).toFixed(2);
+        } else {
+            return -1;
+        }
+    }
+
   return (
     <div className="App">
         <div className="container">
             <h1 className="heading">Aktywność sportowa</h1>
+            <h1 className="subheading">{!countdown.finished ? `Do końca wakacji: ${countdown.days}d ${countdown.hours}h ${countdown.minutes}m ${countdown.seconds}s (${countdown.percent}%)` : "Po czasie!"}</h1>
             <div className="activities">
-                {getActivity("Ride") && <Activity distance={getActivity("Ride").distance} goal={getActivity("Ride").goal} ic="fa-bicycle" name="Jazda na rowerze" color="#639fff" />}
-                {getActivity("Walk") && <Activity distance={getActivity("Walk").distance} goal={getActivity("Walk").goal} ic="fa-person-walking" name="Spacerowanie" color="#ff9b00" />}
+                {getActivity("Ride") && <Activity data={getActivity("Ride")} ic="fa-bicycle" name="Jazda na rowerze" color="#639fff" avg={countAvg(getActivity("Ride"))} />}
+                {getActivity("Walk") && <Activity data={getActivity("Walk")} ic="fa-person-walking" name="Spacerowanie" color="#ff9b00" avg={countAvg(getActivity("Walk"))} />}
             </div>
             <span className="update">Ostatnia aktualizacja: {data.updateTime ? toDate(data.updateTime) : "Nie załadowano danych"}<br/>Status autoryzacji: {data.auth ? "OK" : "Potrzebna autoryzacja"}</span>
         </div>
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,7 @@
 import './Activity.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Activity({name, ic, distance, goal, color}) {
+function Activity({name, ic, data, color, avg}) {
     return (
         <div className="activity">
             <div className="up">
@@ -11,10 +11,13 @@ function Activity({name, ic, distance, goal, color}) {
                     </span>
                     <span className="medium">{name}</span>
                 </div>
-                <span className="light">{distance.toFixed(3)} km/{goal} km</span>
+                <span className="light">{data.distance.toFixed(3)} km/{data.goal} km</span>
             </div>
             <div className="animated-progress">
-                <span style={{"width": ((distance/goal)*100) + "%", "background-color": color}}></span>
+                <span style={{"width": ((data.distance/data.goal)*100) + "%", "background-color": color}}></span>
+            </div>
+            <div className="up">
+                {avg !== -1 && <span className="light2">{`Średnio ${avg}km dziennie aby osiągnąć cel.`}</span>}
             </div>
         </div>
     );
