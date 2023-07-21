5171ed37f7ccbe6becca1b53e401999585539b08
rkubapl <kontakt@rkuba.pl>
2023-07-21 12:06:45 +0200

    Added percent

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,18 @@
 import './Activity.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 function Activity({name, ic, data, color, avg}) {
+    function percent() { return ((data.distance/data.goal)*100) };
+
+    function toDecimalStr(value) {
+        let str=value.toFixed(1).replace(/([0]+)$/,"");
+        try {
+            if (str.endsWith(".")) str+='0';
+        } catch (e) {
+            str+='0';
+        }
+        return str;
+    }
+
     return (
         <div className="activity">
             <div className="up">
@@ -11,10 +22,11 @@ function Activity({name, ic, data, color, avg}) {
                     </span>
                     <span className="medium">{name}</span>
                 </div>
-                <span className="light">{data.distance.toFixed(3)} km/{data.goal} km</span>
+                <span className="light">{parseFloat(data.distance.toFixed(3))} km/{data.goal} km</span>
             </div>
             <div className="animated-progress">
-                <span style={{"width": ((data.distance/data.goal)*100) + "%", "background-color": color}}></span>
+                <span className="progress-span">{parseFloat(percent().toFixed(1))}%</span>
+                <span className="progress" style={{"width": percent() + "%", "background-color": color}}></span>
             </div>
             <div className="up">
                 {avg !== -1 && <span className="light2">{`Średnio ${avg}km dziennie aby osiągnąć cel.`}</span>}
